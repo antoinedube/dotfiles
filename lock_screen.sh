116739d980b3828c5340d2ab2a86f603a621b19e
Antoine Dubé <antoine.dube@dti.ulaval.ca>
2019-11-15 10:14:25 -0500

    Delete previous screenshot of lockscreen

--- lock_screen.sh
+++ lock_screen.sh
@@ -3,6 +3,9 @@
 SCREENSHOT='/tmp/scrot_lock_screen.png'
 BLURRED_SCREENSHOT='/tmp/scrot_lock_screen_blurred.png'
 
+rm $SCREENSHOT
+rm $BLURRED_SCREENSHOT
+
 scrot -m -q 50 $SCREENSHOT
 convert $SCREENSHOT -blur 0x6 -brightness-contrast -10x-10 $BLURRED_SCREENSHOT
 i3lock -i $BLURRED_SCREENSHOT

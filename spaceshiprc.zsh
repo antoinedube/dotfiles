4ad8e8e2bf57602eb3c8ebfe27b9711581d8657e
Antoine Dubé <antoine.dube@gmail.com>
2025-07-31 10:39:28 -0400

    Increase exec_time elapsed

--- spaceshiprc.zsh
+++ spaceshiprc.zsh
@@ -1,23 +1,20 @@
 # Ref: https://spaceship-prompt.sh/sections/
 
-# Theme config - fixed syntax
-# ZSH_THEME="spaceship"
-
 # Spaceship settings (fixed syntax)
 SPACESHIP_PROMPT_ASYNC=true
 SPACESHIP_PROMPT_ADD_NEWLINE=false
 SPACESHIP_RPROMPT_ADD_NEWLINE=true
-
-SPACESHIP_CHAR_SYMBOL=" ⚡ "
+SPACESHIP_CHAR_SYMBOL="⚡ "
 
 # exec_time
+SPACESHIP_EXEC_TIME_ELAPSED=10
 SPACESHIP_EXEC_TIME_PRECISION=2
 
 # exit_code
 SPACESHIP_EXIT_CODE_SHOW=true
 
 # git
-SPACESHIP_GIT_ORDER=(git_status git_branch git_commit)
+SPACESHIP_GIT_ORDER=(git_status git_branch)
 SPACESHIP_GIT_COMMIT_SHOW=true
 SPACESHIP_GIT_STATUS_SUFFIX="] "
 

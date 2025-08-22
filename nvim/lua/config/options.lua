9b4a163b6ee76e3855248d9b221b7a890333c528
Antoine Dubé <antoine.dube@gmail.com>
2025-08-22 19:41:10 -0400

    Updated config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,9 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
 
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 
-vim.opt.textwidth = 160
-
-vim.opt.wrap = true
+opt.textwidth = 160
+opt.wrap = true
+opt.spelllang = { "en", "fr" }

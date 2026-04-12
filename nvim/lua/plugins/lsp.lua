5a96f36c378805fc31352f14afdde85ed8b792f5
Antoine Dubé <antoine.dube@gmail.com>
2026-04-12 08:43:33 -0400

    Update config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
     {
         "neovim/nvim-lspconfig",
         opts = {
+            inlay_hints = { enabled = false },
             servers = {
                 -- copilot.lua only works with its own copilot lsp server
                 copilot = { enabled = false },
